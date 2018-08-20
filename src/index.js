214b2643f187a1b07725278b21778b0a1f30ace8
Seonghun <lutis107@gmail.com>
2018-08-20 21:08:15 +0900

    Create store

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
 
+// 리덕스 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(<App />, document.getElementById('root'));
