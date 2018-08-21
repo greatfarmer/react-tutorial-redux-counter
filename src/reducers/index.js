0cccf97485af1bbda5969727780c485a2691afae
Seonghun <lutis107@gmail.com>
2018-08-21 13:42:04 +0900

    Define initialState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,11 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
-
-/*
-  서브 리듀서들을 하나로 합칩니다.
-  combineReducers를 실행하고 나면, 나중에 store 형태를
-  파라미터로 전달한 객체 모양대로 만듭니다.
-  지금은 다음과 같이 만듭니다.
-  {
-    numberData: {
+// 초기 상태를 정의합니다.
+const initialState = {
+  counters: [
+    {
+      color: 'black',
       number: 0
-    },
-    colorData: {
-      color: 'black'
     }
-  }
-*/
-
-const reducers = combineReducers({
-  numberData: number,
-  colorData: color
-});
-
-export default reducers;
+  ]
+};
