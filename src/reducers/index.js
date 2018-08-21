0f5595b88369a93e0faabf9b5fb2d47018687000
Seonghun <lutis107@gmail.com>
2018-08-21 14:05:58 +0900

    Add 'INCREMENT', 'DECREMENT', and 'SET_COLOR' actions in 'counter' function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -29,6 +29,39 @@ function counter(state = initialState, action) {
       return {
         counters: counters.slice(0, counters.length - 1)
       };
+    case types.INCREMENT:
+      return {
+        counters: [
+          ...counters.slice(0, action.index), // 선택한 인덱스의 전 아이템들
+          {
+            ...counters[action.index], // 기존 객체에
+            number: counters[action.index].number + 1 // 새 number 값 덮어쓰기
+          },
+          ...counters.slice(action.index + 1, counters.length) // 선택한 인덱스의 다음 아이템들
+        ]
+      };
+    case types.DECREMENT:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number - 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+    case types.SET_COLOR:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            color: action.color
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
     default:
       return state;
   }
