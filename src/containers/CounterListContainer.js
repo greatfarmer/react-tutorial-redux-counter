ec394b2395f58c7f6174e3cf066a8d9df8bcd415
Seonghun <lutis107@gmail.com>
2018-08-21 18:41:56 +0900

    Import 'getRandomColor'

--- src/containers/CounterListContainer.js
+++ src/containers/CounterListContainer.js
@@ -1,31 +1,7 @@
 import Counter from '../components/CounterList';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
-
-// 13가지 색상 중 랜덤으로 선택하는 함수입니다.
-export function getRandomColor() {
-  const colors = [
-    '#495057',
-    '#f03e3e',
-    '#d6336c',
-    '#ae3ec9',
-    '#7048e8',
-    '#4263eb',
-    '#1c7cd6',
-    '#1098ad',
-    '#0ca678',
-    '#37b24d',
-    '#74b816',
-    '#f59f00',
-    '#f76707'
-  ];
-
-  // 0부터 12까지 랜덤 숫자
-  const random = Math.floor(Math.random() * 13);
-
-  // 랜덤 색상 반환
-  return colors[random];
-}
+import getRandomColor from '../lib/getRandomColor';
 
 // store 안의 state 값을 props로 연결합니다.
 const mapStateToProps = (state) => ({counters: state.counters});
